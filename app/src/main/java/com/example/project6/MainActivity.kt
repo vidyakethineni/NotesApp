7b6aad6711fd355f4a60ee6eb821c0765182047e
vkethine <vkethine@iu.edu>
2023-11-17 16:51:27 -0500

    Update Commit

--- app/src/main/java/com/example/project6/MainActivity.kt
+++ app/src/main/java/com/example/project6/MainActivity.kt
@@ -4,32 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private lateinit var viewModel: NotesViewModel
+    private val viewModel: NotesViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.notes_toolbar)
         setSupportActionBar(toolbar)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        navController = navHostFragment.navController //took out val
+        navController = navHostFragment.navController
         val builder = AppBarConfiguration.Builder(navController.graph)
         val appBarConfiguration = builder.build()
         toolbar.setupWithNavController(navController, appBarConfiguration)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -40,12 +39,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.notesToolbarAddNote -> {
-                navController.navigate(R.id.editNoteFragment)
+                viewModel.onNewNoteClicked()
                 true
             }
             R.id.notesToolbarSignOut -> {
                 viewModel.signOut()
-                navController.navigate(R.id.signInFragment)
                 true
             }
             else -> {
